c21eb1753641d03c0561d87942139567e738d891
johnmrtnz <74370157+johnmrtnz@users.noreply.github.com>
2026-01-09 16:12:15 -0500

    Updates to python and html

--- photobooth.py
+++ photobooth.py
@@ -2,11 +2,16 @@
 from flask import Flask, render_template, Response, send_file
 from picamera2 import Picamera2
 import io
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 picam2 = Picamera2()
 picam2.configure(picam2.create_still_configuration())
+
+# Create photos directory if it doesn't exist
+PHOTOS_DIR = 'photos'
+os.makedirs(PHOTOS_DIR, exist_ok=True)
 
 @app.route('/')
 def index():
@@ -15,11 +20,14 @@ def index():
 @app.route('/capture')
 def capture():
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    filepath = f'/tmp/photo_{timestamp}.jpg'
+    filename = f'Bittersweet_{timestamp}.jpg'
+    filepath = os.path.join(PHOTOS_DIR, filename)
+
     picam2.start()
     picam2.capture_file(filepath)
     picam2.stop()
-    return send_file(filepath, as_attachment=True, download_name=f'photobooth_{timestamp}.jpg')
+
+    return send_file(filepath, as_attachment=True, download_name=filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
